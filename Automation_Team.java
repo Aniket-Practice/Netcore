730ddbae941694649066fb529e75d7d158e1116f
Aniket Practice <waghmareaniket150@gmail.com>
2024-02-26 21:32:02 +0530

    Bug has been Fixed

---
+++ Automation_Team.java
@@ -0,0 +1,12 @@
+package Netcore_Cloud;
+
+/**
+ * Automation_Team
+ */
+public class Automation_Team {
+
+    public static void main(String[] vargs){
+        System.out.println("This is Automation Team.");
+        System.out.println("All the bugs are been fixed");
+    }
+}
